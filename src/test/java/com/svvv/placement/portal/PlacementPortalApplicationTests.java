7eaa4184b270aac836046af043b00fda1d37f262
Sir-Derek <skysingh41203@gmail.com>
2025-03-30 21:32:20 +0530

    Initial Commit

---
+++ src/test/java/com/svvv/placement/portal/PlacementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svvv.placement.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
